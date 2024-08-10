7b37c1dc3f4fe20dd89c2bc64461bec298f66267
AAL-MLOps-Shelby-S <nullterra180@gmail.com>
2024-08-09 23:22:43 -0700

    feat(navbar): create navbar component and insert into main index.vue page

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,30 +1,21 @@
 <template>
-  <nav class="fixed w-full bg-white shadow">
+  <nav class="sticky top-0 w-full bg-[#E9F2F4]">
     <div class="container mx-auto px-4 py-2 flex justify-between">
-      <a
-        href="#"
-        class="px-3 py-2 text-gray-800 bg-red-500"
-        @click.prevent="scrollTo('home')"
-        >Home</a
-      >
-      <a
-        href="#"
-        class="px-3 py-2 text-gray-800 bg-blue-500"
-        @click.prevent="scrollTo('projects')"
-        >Projects</a
-      >
-      <a
-        href="#"
-        class="px-3 py-2 text-gray-800 bg-green-500"
-        @click.prevent="scrollTo('skills')"
-        >Skills</a
-      >
-      <a
-        href="#"
-        class="px-3 py-2 text-gray-800 bg-yellow-500"
-        @click.prevent="scrollTo('contact')"
-        >Contact</a
-      >
+      <div class="container-md flex justify-between items-center">
+        <img src="Shelby_profile_image.jpeg" class="rounded-full h-[50px]" />
+        <span class="text-xl">Seong Kyu Sohn | Portfolio</span>
+      </div>
+      <div class="container-lg flex justify-between">
+        <a
+          v-for="category in categories"
+          :key="category.id"
+          :href="category.href"
+          class="px-3 py-2 text-gray-800 border-2 hover:bg-lime-400"
+          @click="scrollTo(category.href)"
+        >
+          {{ category.name }}
+        </a>
+      </div>
     </div>
   </nav>
 </template>
@@ -34,11 +25,17 @@ import VueScrollTo from 'vue-scrollto';
 
 export default {
   name: 'NavbarComponent',
+  props: {
+    categories: {
+      type: Array,
+      default: () => [],
+    },
+  },
   methods: {
     scrollTo(target) {
-      VueScrollTo.scrollTo(`#${target}`, 500, {
+      VueScrollTo.scrollTo(target, 500, {
         easing: 'ease-in-out',
-        offset: -50, // Adjust the offset value to account for fixed navbar
+        // offset: -66, // Adjust the offset value to account for fixed navbar
       });
     },
   },
