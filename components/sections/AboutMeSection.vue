2788a4843598265cce5f7188749b6c7c2cfeda24
AAL-MLOps-Shelby-S <nullterra180@gmail.com>
2024-08-21 15:18:35 -0700

    refactor(About Me section): extract styling to SASS file for better readability

--- components/sections/AboutMeSection.vue
+++ components/sections/AboutMeSection.vue
@@ -1,55 +1,52 @@
 <template>
-  <section
-    id="aboutme"
-    class="min-h-screen w-full flex flex-col items-center justify-around bg-transparent border-b-black border-b-2"
-  >
+  <section id="aboutme" class="aboutme">
     <!-- section title -->
-    <h2 class="text-5xl text-white m-4">
-      About Me
-      <div class="h-[4px] w-full bg-blue-200" />
+    <h2 class="aboutme__title">
+      {{ sectionTitle }}
+      <div class="aboutme__title__decoration" />
     </h2>
     <!-- section contents -->
-    <div class="w-full grid grid-cols-1 p-4 gap-1 md:grid-cols-12">
+    <div class="aboutme__contents">
       <!-- intro text content (left side in non-mobile) -->
-      <div
-        class="flex flex-col px-5 items-center text-white md:col-start-3 md:col-end-7"
-      >
+      <div class="aboutme__intro-container">
         <!-- main introduction content -->
         <!-- TODO: replace img src -->
-        <img src="Shelby_profile_image.jpeg" class="rounded-full h-40 mb-4" />
-        <div class="text-2xl mx-auto mb-4">{{ mainIntroText }}</div>
+        <img
+          src="Shelby_profile_image.jpeg"
+          class="aboutme__intro__profile-picture"
+        />
+        <div class="aboutme__intro__text-top">{{ introTextTop }}</div>
         <!-- real world capabilities content -->
-        <div class="text-4xl mx-auto mb-3">Real-world Capabilities</div>
-        <div class="text-2xl mx-auto">{{ realWorldCapabilitiesText }}</div>
+        <div class="aboutme__intro__subsection-title">
+          {{ subsectionTitle }}
+        </div>
+        <div class="aboutme__intro__subsection-text">{{ subsectionText }}</div>
         <!-- button to open resumé Google Docs link in new tab -->
-        <button
-          class="my-3 px-2 rounded-full bg-lime-300 text-xl text-slate-500"
-          @click="onClickViewResume"
-        >
-          View Resumé
+        <button class="aboutme__button-resume" @click="onClickViewResume">
+          {{ buttonResumeText }}
         </button>
       </div>
       <!-- skills content (right side in non-mobile) -->
-      <div
-        class="text-2xl flex flex-col justify-center items-center md:col-start-8 md:col-end-11"
-      >
-        <div class="my-3 text-slate-200 text-4xl italic">Frontend Skills</div>
-        <div class="flex flex-col">
+      <div class="aboutme__skills-container">
+        <div class="aboutme__skills-title">{{ skillsTitle }}</div>
+        <div class="aboutme__skills-cards-list">
           <div
             v-for="skill in skills"
             :key="skill.id"
-            class="m-2 p-2 flex items-center text-white"
+            class="aboutme__skills-card"
           >
-            <div class="h-[80px] w-[80px] p-2 items-center">
-              <img :src="skill.src" class="h-full" />
+            <div class="aboutme__skills-card__image-container">
+              <img :src="skill.src" class="aboutme__skills-card__image" />
             </div>
-            <div class="flex flex-col p-2">
-              <div class="text-2xl">{{ skill.text }}</div>
-              <div class="text-xl whitespace-nowrap">
+            <div class="aboutme__skills-card__text-container">
+              <div class="aboutme__skills-card__skill-name">
+                {{ skill.text }}
+              </div>
+              <div class="aboutme__skills-card__skill-yoe">
                 <span class="text-blue-400">{{
                   skill.yearsOfExperience.toString()
                 }}</span>
-                Years of Experience
+                {{ yearsOfExperienceText }}
               </div>
             </div>
           </div>
@@ -64,12 +61,17 @@ export default {
   name: 'AboutMeComponent',
   data() {
     return {
-      mainIntroText:
+      sectionTitle: 'About Me',
+      subsectionTitle: 'Real-world Capabilities',
+      introTextTop:
         "Greetings, my name is Samuel Sohn, a frontend web developer transitioning into an MLOps role. I'm passionate about merging my frontend expertise with AI and DevOps to build innovative, scalable solutions.",
-      realWorldCapabilitiesText:
+      subsectionText:
         'I excel at building products that leverage AI to further human interest. My skills lie in crafting intuitive and well-designed interfaces and I am currently cultivating skills to automate the production pipeline of AI models.',
+      buttonResumeText: 'View Resumé',
       resumeGoogleDocsLink:
         'https://docs.google.com/document/d/1GN6Ip5lOVVcvWyrBQSZdS6mzpYP9y0INtjZ0abHtg3s/edit?usp=sharing',
+      skillsTitle: 'Frontend Skills',
+      yearsOfExperienceText: 'Years of Exp.',
       skills: [
         {
           id: 1,
@@ -111,3 +113,7 @@ export default {
   },
 };
 </script>
+
+<style lang="sass" scoped>
+@import '~/styles/sections/_AboutMeSection.sass'
+</style>
