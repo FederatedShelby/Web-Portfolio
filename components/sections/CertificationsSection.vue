114bd25573181af501a074a33a227f59fc15c2e6
AAL-MLOps-Shelby-S <nullterra180@gmail.com>
2024-08-21 15:44:39 -0700

    refactor(Certifications section): extract styling to SASS file for better readability

--- components/sections/CertificationsSection.vue
+++ components/sections/CertificationsSection.vue
@@ -1,29 +1,33 @@
 <template>
-  <section
-    id="certifications"
-    class="w-full flex flex-col items-center justify-around bg-transparent"
-  >
+  <section id="certifications" class="certifications">
     <!-- section title -->
-    <h2 class="text-5xl text-white m-4">
-      Certifications
-      <div class="h-[4px] w-full bg-blue-200" />
+    <h2 class="certifications__title">
+      {{ sectionTitle }}
+      <div class="certifications__title__decoration" />
     </h2>
     <!-- certifications area -->
-    <div class="w-full p-4 flex justify-center">
-      <!-- certification base component -->
+    <div class="certifications__certifications-cards-list">
+      <!-- certification cards -->
       <div
         v-for="cert in certificationsList"
         :key="cert.id"
-        class="flex p-2 rounded-md bg-white bg-opacity-30 cursor-pointer"
+        class="certifications__certifications-card"
       >
         <!-- image container -->
-        <div class="h-[120px] w-[120px] flex items-center justify-center">
-          <img :src="cert.imageSrc" class="h-full" />
+        <div class="certifications__certifications-card__image-container">
+          <img
+            :src="cert.imageSrc"
+            class="certifications__certifications-card__image"
+          />
         </div>
         <!-- certification text info -->
-        <div class="flex flex-col p-2 text-white">
-          <div class="text-2xl">{{ cert.certificationName }}</div>
-          <div class="text-xl text-slate-100 italic">
+        <div class="certifications__certifications-card__text-container">
+          <div class="certifications__certifications-card__text-cert-name">
+            {{ cert.certificationName }}
+          </div>
+          <div
+            class="certifications__certifications-card__text-cert-date-range"
+          >
             {{ cert.startDate + ' ~ ' + cert.endDate }}
           </div>
         </div>
@@ -37,6 +41,7 @@ export default {
   name: 'CertificationsComponent',
   data() {
     return {
+      sectionTitle: 'Certifications',
       certificationsList: [
         {
           id: 1,
@@ -51,3 +56,7 @@ export default {
   },
 };
 </script>
+
+<style lang="sass" scoped>
+@import '~/styles/sections/_CertificationsSection.sass'
+</style>
