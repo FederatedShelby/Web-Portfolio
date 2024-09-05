30d33a72a61994a9fd2a71f07f06ae08af5a2d6a
AAL-MLOps-Shelby-S <nullterra180@gmail.com>
2024-09-05 16:20:05 -0700

    feat(home): define landing page as Home section as componentized element

--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,39 @@
 <template>
-  <div>
-    <section class="min-h-screen flex flex-col items-center justify-center">
-      <div class="text-5xl flex flex-col items-center">
-        <span class="text-white"
-          >{{ mainTextIntroTop }}
-          <span class="text-blue-400">{{ mainTextName }}</span
-          >.
-        </span>
-        <span class="text-white mt-3">{{ mainTextIntroBottom }}</span>
-      </div>
-    </section>
-
-    <ThreeBackground />
-
+  <div id="Main">
+    <!-- landing page -->
+    <Home />
+    <!-- navbar component -->
     <Navbar />
-
+    <!-- sections -->
     <AboutMe />
-
     <Experience />
-
     <Certifications />
-
     <Projects />
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import Vue from 'vue';
+
 // components
 import Navbar from '~/components/Navbar.vue';
-import ThreeBackground from '~/components/ThreeBackground.vue';
 
 // page sections
+import Home from '~/components/sections/HomeSection.vue';
 import AboutMe from '~/components/sections/AboutMeSection.vue';
 import Experience from '~/components/sections/ExperienceSection.vue';
 import Certifications from '~/components/sections/CertificationsSection.vue';
 import Projects from '~/components/sections/ProjectsSection.vue';
 
-// data variables
-import { appData } from '~/data/app';
-import { navbarData } from '~/data/navbar';
-
 export default Vue.extend({
   name: 'IndexPage',
   components: {
     Navbar,
-    ThreeBackground,
+    Home,
     AboutMe,
     Experience,
     Certifications,
     Projects,
-  },
-  data() {
-    return {
-      mainTextIntroTop: appData.mainTextIntroTop,
-      mainTextName: appData.mainTextName,
-      mainTextIntroBottom: appData.mainTextIntroBottom,
-      categories: navbarData.categories,
-    };
   },
 });
 </script>
