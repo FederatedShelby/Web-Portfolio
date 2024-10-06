af0dc877d8322d3cc481eea7d3396948efe508ed
AAL-MLOps-Shelby-S <nullterra180@gmail.com>
2024-10-06 12:36:49 -0700

    feat(About Me): modify About Me section to include only profile picture, first intro text, and resume/talk btns

--- components/sections/AboutMeSection.vue
+++ components/sections/AboutMeSection.vue
@@ -1,65 +1,64 @@
 <template>
   <section id="aboutme" class="aboutme">
-    <!-- section title -->
-    <h2 class="title" data-aos="flip-up">
-      {{ sectionTitle }}
-      <div class="title-decoration" />
-    </h2>
     <!-- section contents -->
     <div class="aboutme__contents">
-      <!-- intro text content (left side in non-mobile) -->
-      <div class="intro-container" data-aos="fade-right" data-aos-once="true">
-        <!-- main introduction content -->
-        <div class="intro__text-top">
+      <!-- intro text content -->
+      <div class="intro-container">
+        <!-- profile image -->
+        <div
+          class="intro__profile-picture"
+          style="background-image: url('Shelby_profile_image.svg')"
+          data-aos="fade-up"
+          data-aos-once="true"
+        />
+        <!-- introduction text -->
+        <div class="intro__text" data-aos="fade-up" data-aos-once="true">
           <span
-          v-for="textObj in introTopTextList"
-          :key="textObj.id"
-          :class="textObj.weight === 1 ? 'font-normal' : 'font-thin'"
-          >{{ textObj.text }}</span>
+            v-for="textObj in introTextList"
+            :key="textObj.id"
+            :class="textObj.weight === 1 ? 'font-normal' : 'font-thin'"
+            >{{ textObj.text }}</span
+          >
         </div>
-        <!-- real world capabilities content -->
-        <div class="intro__subsection-title">
-          {{ subsectionTitle }}
+        <div
+          class="intro__buttons"
+          data-aos="fade-right"
+          data-aos-once="true"
+          data-aos-delay="300"
+        >
+          <button class="button-resume" @click="onClickViewResume">
+            {{ buttonResumeText }}
+          </button>
+          <button class="button-talk" @click="onClickLetsTalk">
+            {{ buttonTalkText }}
+          </button>
         </div>
-        <div class="intro__subsection-text">
-          <span
-          v-for="textObj in subsectionTextList"
-          :key="textObj.id"
-          :class="textObj.weight === 1 ? 'font-normal' : 'font-thin'"
-          >{{ textObj.text }}</span>
-        </div>
-        <!-- button to open resumé Google Docs link in new tab -->
-        <button class="button-resume" @click="onClickViewResume">
-          {{ buttonResumeText }}
-        </button>
       </div>
     </div>
+    <!-- TODO: socials (email, linkedin, github) -->
   </section>
 </template>
 
 <script>
 // data variables
-import { navbarData, CATEGORIES_INDEX_ABOUTME } from '~/data/navbar';
 import { aboutmeData } from '~/data/sections/aboutme';
 
 export default {
   name: 'AboutMeComponent',
   data() {
     return {
-      sectionTitle: navbarData.categories[CATEGORIES_INDEX_ABOUTME].name,
-      subsectionTitle: aboutmeData.subsectionTitle,
-      introTopTextList: aboutmeData.introTopTextList,
-      subsectionTextList: aboutmeData.subsectionTextList,
+      introTextList: aboutmeData.introTextList,
       buttonResumeText: aboutmeData.buttonResumeText,
+      buttonTalkText: aboutmeData.buttonTalkText,
       resumeGoogleDocsLink: aboutmeData.resumeGoogleDocsLink,
-      skillsTitle: aboutmeData.skillsTitle,
-      yearsOfExperienceText: aboutmeData.yearsOfExperienceText,
-      skills: aboutmeData.skills,
     };
   },
   methods: {
     onClickViewResume() {
       window.open(this.resumeGoogleDocsLink, '_blank', 'noopener noreferrer');
+    },
+    onClickLetsTalk() {
+      window.location.href = 'mailto:nullterra180@gmail.com';
     },
   },
 };
