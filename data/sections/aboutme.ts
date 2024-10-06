6096e771efbcb300c3af38973dea22d672e6efd5
AAL-MLOps-Shelby-S <nullterra180@gmail.com>
2024-10-06 12:35:20 -0700

    feat(skills data): extract skills section data to own file from aboutme

--- data/sections/aboutme.ts
+++ data/sections/aboutme.ts
@@ -1,15 +1,11 @@
-const currentDate: Date = new Date();
-// 2 years removed from dates' difference to account for correct calculation
-const yoeBuffer: number = 2;
-
 // Data used to populate About Me section contents
 export const aboutmeData: any = Object.freeze({
   // PATH:  components > sections > AboutMeSection.vue
   // USAGE: "About Me" content
-  subsectionTitle: 'Real-world Capabilities',
+
   // weight 0 = default
   // weight 1 = font-weight: bold
-  introTopTextList: [
+  introTextList: [
     {
       id: 1,
       text: 'I am a ',
@@ -17,12 +13,12 @@ export const aboutmeData: any = Object.freeze({
     },
     {
       id: 2,
-      text: ' frontend developer',
+      text: ' frontend software engineer',
       weight: 1,
     },
     {
       id: 3,
-      text: ' aiming to transition into',
+      text: ' with a strong interest in',
       weight: 0,
     },
     {
@@ -32,103 +28,17 @@ export const aboutmeData: any = Object.freeze({
     },
     {
       id: 5,
-      text: ". I'm passionate about merging my frontend expertise with",
+      text: ', passionate about building',
       weight: 0,
     },
     {
       id: 6,
-      text: ' AI',
-      weight: 1,
-    },
-    {
-      id: 7,
-      text: ' and',
-      weight: 0,
-    },
-    {
-      id: 8,
-      text: ' DevOps',
-      weight: 1,
-    },
-    {
-      id: 9,
-      text: ' to build innovative, scalable solutions that create impact in the real world.',
-      weight: 0,
-    },
-  ],
-  subsectionTextList: [
-    {
-      id: 1,
-      text: 'I excel at building products that leverage AI',
-      weight: 0,
-    },
-    {
-      id: 2,
-      text: ' to further human interest and understanding',
-      weight: 1,
-    },
-    {
-      id: 3,
-      text: '. My skills lie in crafting intuitive and well-designed interfaces and I am currently',
-      weight: 0,
-    },
-
-    {
-      id: 4,
-      text: ' cultivating new and existing skills',
-      weight: 1,
-    },
-    {
-      id: 5,
-      text: ' to better automate and visualize the production pipeline of AI models.',
+      text: ' innovative, impactful solutions in the real world.',
       weight: 0,
     },
   ],
   buttonResumeText: 'View Resumé',
+  buttonTalkText: "Let's talk",
   resumeGoogleDocsLink:
     'https://docs.google.com/document/d/1GN6Ip5lOVVcvWyrBQSZdS6mzpYP9y0INtjZ0abHtg3s/edit?usp=sharing',
-  skillsTitle: 'Skills',
-  categoriesList: [
-    {
-      id: 1,
-      name: 'Languages',
-      skillsList: [
-        'JavaScript (ES6)',
-        'TypeScript',
-        'HTML',
-        'CSS / SASS',
-        'Python',
-        'SQL',
-      ],
-    },
-    {
-      id: 2,
-      name: 'Frameworks',
-      skillsList: ['Vue.js', 'Nuxt', 'React', 'WordPress', 'Jest', 'JMeter'],
-    },
-    {
-      id: 3,
-      name: 'Tools',
-      skillsList: [
-        'Git & GitHub',
-        'AWS Console',
-        'Chrome DevTools',
-        'Postman',
-        'ArcGIS',
-        'MongoDB',
-      ],
-    },
-    {
-      id: 4,
-      name: 'Design & Documentation',
-      skillsList: [
-        'UML',
-        'Figma',
-        'LucidChart',
-        'Storybook',
-        'Notion',
-        'Prototyping',
-      ],
-    },
-  ],
 });
---
+++ data/sections/skills.ts
@@ -0,0 +1,48 @@
+// Data used to populate About Me section contents
+export const skillsData: any = Object.freeze({
+  // PATH:  components > sections > SkillsSection.vue
+  // USAGE: "Skills" content
+  categoriesList: [
+    {
+      id: 1,
+      name: 'Languages',
+      skillsList: [
+        'JavaScript (ES6)',
+        'TypeScript',
+        'HTML',
+        'CSS / SASS',
+        'Python',
+        'SQL',
+      ],
+    },
+    {
+      id: 2,
+      name: 'Frameworks',
+      skillsList: ['Vue.js', 'Nuxt', 'React', 'WordPress', 'Jest', 'JMeter'],
+    },
+    {
+      id: 3,
+      name: 'Tools',
+      skillsList: [
+        'Git & GitHub',
+        'AWS Console',
+        'Chrome DevTools',
+        'Postman',
+        'ArcGIS',
+        'MongoDB',
+      ],
+    },
+    {
+      id: 4,
+      name: 'Design & Documentation',
+      skillsList: [
+        'UML',
+        'Figma',
+        'LucidChart',
+        'Storybook',
+        'Notion',
+        'Prototyping',
+      ],
+    },
+  ],
+});
