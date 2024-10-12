e81713850ebf6f3975f64ca52d9b20c44b38c169
AAL-MLOps-Shelby-S <nullterra180@gmail.com>
2024-10-12 15:01:10 -0700

    feat(IconLock): create lock icon component for conditionally rendering lock/unlock svg based on dark/light mode and isLocked property

---
+++ components/IconLock.vue
@@ -0,0 +1,35 @@
+<template>
+  <img
+    :src="getImgSrcPath"
+    width="18"
+    height="18"
+    alt="lock-icon"
+    loading="lazy"
+  >
+</template>
+
+<script>
+// libraries
+import { mapState } from 'vuex';
+
+export default {
+  name: 'IconLock',
+  props: {
+    isLocked: {
+      type: Boolean,
+      default: false,
+    }
+  },
+  computed: {
+    ...mapState({
+      isDarkMode: (state) => state.isDarkMode,
+    }),
+    getImgSrcPath() {
+      if (this.isLocked) {
+        return this.isDarkMode ? 'icon-lock__dark.svg' : 'icon-lock__light.svg';
+      }
+      return this.isDarkMode ? 'icon-unlock__dark.svg' : 'icon-unlock__light.svg';
+    }
+  }
+};
+</script>
